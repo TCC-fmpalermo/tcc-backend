65926fdc6dbb60e98ec4c9c5239f689d6f0c3d62
Francisco <franciscoctism@gmail.com>
2024-11-28 18:19:36 -0300

    chore: remove log

--- src/desktop-options/desktop-options.controller.ts
+++ src/desktop-options/desktop-options.controller.ts
@@ -9,8 +9,6 @@ export class DesktopOptionsController {
 
   @Post()
   create(@Body() createDesktopOptionDto: CreateDesktopOptionDto) {
-    console.log(createDesktopOptionDto);
-    
     return this.desktopOptionsService.create(createDesktopOptionDto);
   }
 
